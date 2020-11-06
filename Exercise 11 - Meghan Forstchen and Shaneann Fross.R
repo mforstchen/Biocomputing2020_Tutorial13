9dcaa2e78334ba20f809f996192709304f346c5d
mforstchen <71297558+mforstchen@users.noreply.github.com>
2020-11-06 14:48:20 -0500

    Update Exercise 11 - Meghan Forstchen and Shaneann Fross.R
    Create a dataframe to store output and initial values

--- Exercise 11 - Meghan Forstchen and Shaneann Fross.R
+++ Exercise 11 - Meghan Forstchen and Shaneann Fross.R
@@ -7,3 +7,8 @@ M0=1
 r=0.1
 K=1000000
 timesteps=100
+
+#Create a dataframe to store output and initial values
+NS=data.frame(time=1:timesteps, sim1=rep(0,timesteps), sim2=rep(0,timesteps))
+NS$sim1[1]=N0
+NS$sim2[1]=M0
